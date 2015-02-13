4f0c323186dfc68da0e9cb3d152c1163be9c1f47
solomreb <solomreb@onid.oregonstate.edu>
2015-02-13 15:34:21 -0800

    definition.sql done

--- assignment2-definition.sql
+++ assignment2-definition.sql
@@ -34,7 +34,7 @@ CREATE TABLE operating_system
  name VARCHAR(255) NOT NULL,
  version VARCHAR(255) NOT NULL,
  CONSTRAINT version_unique UNIQUE (name, version)
-)ENGINE = InnoDB;
+);
 
 
 -- Create a table called device with the following properties:
@@ -52,7 +52,7 @@ CREATE TABLE device
  received DATE,
  isbroken BOOLEAN,
  FOREIGN KEY(cid) REFERENCES category_tbl(id)
-)ENGINE = InnoDB;
+);
 
 
 
@@ -71,7 +71,7 @@ CREATE TABLE os_support
     FOREIGN KEY(did) REFERENCES device(id),
     FOREIGN KEY(osid) REFERENCES operating_system(id),
     PRIMARY KEY (did, osid)
-)ENGINE = InnoDB;
+);
 
 
 -- insert the following into the category_tbl table:
@@ -118,28 +118,52 @@ VALUES ('iOS','4.0');
 -- received - 1/2/1970
 -- isbroken - True
 
+INSERT INTO device (cid, name, received, isbroken) 
+    SELECT id, 'Samsung Atlas', DATE '1970-01-02', true
+    FROM category_tbl 
+    WHERE name='phone' AND subcategory='maybe a tablet?';
+
 -- cid - reference to name: tablet subcategory: but kind of a laptop
 -- name - Nokia
 -- received - 5/6/1999
 -- isbroken - False
 
+INSERT INTO device (cid, name, received, isbroken)
+    SELECT id, 'Nokia', DATE '1999-05-06', false
+    FROM category_tbl
+    WHERE name='tablet' AND subcategory='but kind of a laptop';
+
 -- cid - reference to name: tablet subcategory: ereader
 -- name - jPad
 -- received - 11/18/2005
 -- isbroken - False
-
+INSERT INTO device (cid, name, received, isbroken)
+    SELECT id, 'jPad', DATE '2005-11-18', false
+    FROM category_tbl
+    WHERE name='tablet' AND subcategory='ereader';
 
 -- insert the following into the os_support table using subqueries to look up data as needed:
 -- device: Samsung Atlas
 -- os: Android 1.0
 -- notes: Works poorly
+INSERT INTO os_support (did, osid, notes)
+    VALUES((SELECT id FROM device WHERE name='Samsung Atlas'),
+           (SELECT id FROM operating_system WHERE name='Android' AND version='1.0'),
+           'Works poorly');
 
 -- device: Samsung Atlas
 -- os: Android 2.0
 -- notes: NULL
+INSERT INTO os_support (did, osid)
+    VALUES((SELECT id FROM device WHERE name='Samsung Atlas'),
+           (SELECT id FROM operating_system WHERE name='Android' AND version='2.0'));
+
 
 -- device: jPad
 -- os: iOS 4.0
 -- notes: NULL
+INSERT INTO os_support (did, osid)
+    VALUES((SELECT id FROM device WHERE name='jPad'),
+           (SELECT id FROM operating_system WHERE name='iOS' AND version='4.0'));
 
 
