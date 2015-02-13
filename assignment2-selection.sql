3cfe518abebb1c751436a1171ec2250daba80188
solomreb <solomreb@onid.oregonstate.edu>
2015-02-12 21:28:34 -0800

    added template files

---
+++ assignment2-selection.sql
@@ -0,0 +1,24 @@
+#1 Find the film title and category name of all films in which SANDRA PECK acted
+#Order the results by title, descending (use ORDER BY title DESC at the end of the query)
+
+
+
+#2 We want to find out how many of each category of film each actor has started in so return a table with actor's id, actor's first name, actor's last name, category name and the count
+#of the number of films that the actor was in which were in that category. Order the results by category name ascending. (Please pay attention that there may be many actors with the same last names, and also there may be few actors with the exact same first names and last names)
+
+
+
+#3 Find the first name, last name and total combined film length of Action films for every actor whose last name starts with 'B'.
+#That is the result should list the names of actors and the total lenght of Action films they have been in.(Your query should also list those actors whose last names start with 'B', but never acted in an Action film.)
+
+
+
+#4 Find the first name and last name of all actors who have never been in an Action film that has a length of more than 100 minutes.
+
+
+
+#5 Find the film title of all films which feature both KIRSTEN PALTROW and WARREN NOLTE
+#Order the results by title, descending (use ORDER BY title DESC at the end of the query)
+#Warning, this is a tricky one and while the syntax is all things you know, you have to think oustide
+#the box a bit to figure out how to get a table that shows pairs of actors in movies
+
